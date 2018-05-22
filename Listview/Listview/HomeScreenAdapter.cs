49ffbb655da6fe8829345c5d58e0e1cf299d12a8
Edgar112 <edgar4443@live.com>
2018-05-22 16:00:57 +0300

    VALm1s

---
+++ Listview/Listview/HomeScreenAdapter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Listview
+{
+    public class HomeScreenAdapter : BaseAdapter<TableItem>
+    {
+        public List<TableItem> mItems;
+        private Context mContext;
+        public HomeScreenAdapter(Context context, List<TableItem> items)
+        {
+            mItems = items;
+            mContext = context;
+           
+        }
+        public override int Count
+        {
+            get { return mItems.Count; }
+        }
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+        public override TableItem this[int position]
+        {
+            get { return mItems[position]; }
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+
+            View row = convertView;
+            if (row == null)
+                row = LayoutInflater.From(mContext).Inflate(Resource.Layout.CustomView, null, false);
+
+            var item = mItems[position];
+
+            TextView txtTootja = row.FindViewById<TextView>(Resource.Id.Tootja);
+            TextView txtMudel = row.FindViewById<TextView>(Resource.Id.Mudel);
+            TextView txtKilovatid = row.FindViewById<TextView>(Resource.Id.Kilovatid);
+            ImageView pilt = row.FindViewById<ImageView>(Resource.Id.Image1);
+
+            txtTootja.Text = mItems[position].Tootja;
+            txtMudel.Text = mItems[position].Mudel;
+            txtKilovatid.Text = mItems[position].Kilovatid;
+            pilt.SetImageResource(mItems[position].AutoPilt);
+            return row;
+        }
+    }
+}
---
+++ Listview/Listview/MainActivity.cs
@@ -0,0 +1,99 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Collections.Generic;
+
+namespace Listview
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private List<TableItem> mItems;
+        private ListView mListView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+            mListView = FindViewById<ListView>(Resource.Id.listView1);
+
+            mItems = new List<TableItem>();
+
+            mItems.Add(new TableItem()
+            {
+                AutoPilt = Resource.Drawable.car1, // 1
+                Tootja = "BMW",
+                Mudel = "DDAe2",
+                Kilovatid = "223kw"
+            });
+            mItems.Add(new TableItem()
+            {
+                AutoPilt = Resource.Drawable.car2, // 2
+                Tootja = "BMW",
+                Mudel = "Gaed",
+                Kilovatid = "228kw"
+            });
+            mItems.Add(new TableItem()
+            {
+                AutoPilt = Resource.Drawable.car3, // 3
+                Tootja = "BMW",
+                Mudel = "GAWfg23",
+                Kilovatid = "148kw"
+            });
+            mItems.Add(new TableItem()
+            {
+                AutoPilt = Resource.Drawable.car4, // 4
+                Tootja = "BMW",
+                Mudel = "11sWfg23",
+                Kilovatid = "151kw"
+            });
+            mItems.Add(new TableItem() //5
+            {
+                AutoPilt = Resource.Drawable.car5, 
+                Tootja = "BMW",
+                Mudel = "11qq2g23",
+                Kilovatid = "199kw"
+            });
+            mItems.Add(new TableItem()
+            {
+                AutoPilt = Resource.Drawable.car6,
+                Tootja = "BMW",
+                Mudel = "1JFaEE3",
+                Kilovatid = "151kw"
+            });
+            mItems.Add(new TableItem()
+            {
+                AutoPilt = Resource.Drawable.car7,
+                Tootja = "Nissan",
+                Mudel = "Micbuss",
+                Kilovatid = "151kw"
+            });
+            mItems.Add(new TableItem()
+            {
+                AutoPilt = Resource.Drawable.car8,
+                Tootja = "Nissan",
+                Mudel = "MicBus2020",
+                Kilovatid = "1200kw"
+            });
+            mItems.Add(new TableItem()
+            {
+                AutoPilt = Resource.Drawable.car9,
+                Tootja = "Nissan",
+                Mudel = "LowKBuss",
+                Kilovatid = "11kw"
+            });
+            mItems.Add(new TableItem()
+            {
+                AutoPilt = Resource.Drawable.car10,
+                Tootja = "Nissan",
+                Mudel = "UltraJapanMX-3",
+                Kilovatid = "90kw"
+            });
+            HomeScreenAdapter adapter = new HomeScreenAdapter(this, mItems);
+            mListView.Adapter = adapter;
+        }
+    } 
+}
+
